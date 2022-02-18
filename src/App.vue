ffb33f46e94e47bf9568eb67bfb38e2989d07657
yassinedef <yassinedef@gmail.com>
2022-02-18 21:29:53 +0100

    premier comit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from './components/dossierB/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <HelloWorld/>
+    </div>
+  </header>
+
+ 
+</template>
+
+<style>
+@import './assets/base.css';
+
+</style>
---
+++ src/components/dossierB/HelloWorld.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+ <div>
+  <h1> Naas nouss</h1>
+  </div>
+</template>
+
+<style scoped>
+
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+
+h1 {
+  display: flex;
+  justify-content: center;
+}
+</style>
